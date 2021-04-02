8ca7dc4e4ac77dd391e4b5d70aabc60a7dcebde5
Ashwin Khode <ashwin.khode1@gmail.com>
2021-04-03 05:08:31 +0530

    fix: Badge component fixed

--- src/components/Badge/Badge.js
+++ src/components/Badge/Badge.js
@@ -1,6 +1,6 @@
 const Badge = ({query}) => {
     return (
-        <div className="absolute -top-2 right-0 sm:-top-1 sm:right-2 text-[0.5rem] text-white font-bold px-[4px] sm:p-1 rounded-full bg-red-500 flex justify-center items-center">{query}</div>
+        <div className="absolute -top-2 right-0 sm:-top-1 sm:right-2 text-[0.5rem] text-white font-bold px-[4px] rounded-full bg-red-500 flex justify-center items-center">{query}</div>
     )
 }
 
