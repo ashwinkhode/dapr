6354cdd3158377ac53384eeeada93cbf0c1621f6
Ashwin Khode <ashwin.khode1@gmail.com>
2021-04-03 17:18:17 +0530

    fix: ProductCard & ProductGrid fixed

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,37 +1,44 @@
 import Image from 'next/image'
 import {useState} from 'react'
 import {AiOutlineHeart, AiFillHeart} from 'react-icons/ai'
+import Button from '../Button/Button'
 
-const ProductCard = () => {
+const ProductCard = ({product: {id, title, price, description, category, image}}) => {
     const [state, setState] = useState(false)
     return (
         // <div className="flex flex-col w-1/2 sm:max-w-[14rem] text-sm sm:text-base min-w-[6rem] md:min-w-[12rem] lg:min-w-[12rem] sm:rounded sm:shadow-md border border-gray-300">
-        <div className="w-1/2 overflow-hidden md:my-1 md:px-1 md:w-1/3 lg:my-2 lg:px-1 lg:w-1/5 xl:my-2 xl:px-1 xl:w-1/5 sm:shadow-lg sm:hover:shadow-2xl sm:transition-shadow">
+        <div className="w-1/2 flex flex-col justify-between overflow-hidden md:my-1 md:px-1 md:w-1/3 lg:my-2 lg:px-1 lg:w-1/5 xl:my-2 xl:px-1 xl:w-1/5 sm:shadow-lg sm:hover:shadow-2xl sm:transition-shadow">
             <div className="relative w-full overflow-hidden shadow-sm">
-                <button className="absolute top-2 right-2 bg-white rounded-full p-2 z-10 focus:outline-none outline-none"
-                    onClick={() => setState(!state)}
-                >
-                    {
-                        state ? <AiFillHeart className='text-red-500' /> : <AiOutlineHeart />
-                    }
-                </button>
-                <span className='absolute bg-white text-xs font-bold rounded-sm top-2 left-2 px-2 py-1 z-10'>-49%</span>
-                <img
-                    className='min-w-full min-h-full object-fill'
-                    src='/product.jpeg'
-                    alt='product'
-                />
+                <div>
+                    <button className="absolute top-2 right-2 bg-white rounded-full p-2 z-10 focus:outline-none outline-none"
+                        onClick={() => setState(!state)}
+                    >
+                        {
+                            state ? <AiFillHeart className='text-red-500' /> : <AiOutlineHeart />
+                        }
+                    </button>
+                    <span className='absolute bg-gray-200  text-xs font-bold rounded-sm top-2 left-2 px-2 py-1 z-10'>-49%</span>
+                    <img
+                        className='p-2 min-w-full h-[40vh] min-h-full object-fill'
+                        src={image}
+                        alt={title}
+                    />
+                </div>
+                <div className='relative px-4 py-2 w-full'>
+                    <h1 className='font-bold text-base sm:text-xl capitalize opacity-80'>{category}</h1>
+                    <p className='my-1 text-xs sm:text-sm text-gray-500 '>{title}</p>
+                </div>
             </div>
-            <div className="description p-4">
-                <h1 className='font-bold text-base sm:text-xl opacity-80'>Robbie Jones</h1>
-                <p className='my-1 text-xs sm:text-sm text-gray-500'>Casual Sneakers Shoes For Men Sneakers For Men</p>
-                <p className='font-bold text-base sm:text-xl mb-2'>Rs. 2,999
-                <span className='font-normal opacity-60 text-xs sm:text-sm line-through ml-2'>
-                        Rs. 5,999
-                </span>
+            <div className="description p-4 flex flex-col justify-between">
 
-                </p>
-                <button className='w-full bg-gray-700 text-white py-2'>Add to Cart</button>
+                <div>
+                    <p className='font-bold text-base sm:text-xl mb-2'>${price}
+                        <span className='font-normal opacity-60 text-xs sm:text-sm line-through ml-2'>
+                            ${+price + 100}
+                        </span>
+                    </p>
+                </div>
+                <Button variant='primary' >Add to Cart</Button>
             </div>
         </div>
     )
--- src/components/ProductGrid/ProductGrid.js
+++ src/components/ProductGrid/ProductGrid.js
@@ -1,17 +1,14 @@
 import ProductCard from '../ProductCard/ProductCard'
 
-const ProductGrid = () => {
+const ProductGrid = ({products}) => {
     return (
         // <div className="flex flex-row flex-wrap md:justify-between my-4 mx-auto sm:w-11/12 sm:gap-4 flex-1">
         <div className="flex flex-wrap overflow-hidden md:-mx-1 lg:-mx-1 xl:-mx-1">
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-
+            {
+                products.map(
+                    (product) => <ProductCard key={product.id} product={product} />
+                )
+            }
         </div>
 
     )
