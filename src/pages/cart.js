ee4a913b163555e9b08eb18c5b2e56a476f32fa0
Ashwin Khode <ashwin.khode1@gmail.com>
2021-04-03 22:04:46 +0530

    init: cart & wishlist pages initialized

---
+++ src/pages/cart.js
@@ -0,0 +1,9 @@
+const Cart = () => {
+    return (
+        <div>
+            This is cart
+        </div>
+    )
+}
+
+export default Cart
---
+++ src/pages/wishlist.js
@@ -0,0 +1,9 @@
+const Wishlist = () => {
+    return (
+        <div>
+            This is wishlist
+        </div>
+    )
+}
+
+export default Wishlist
