41dcca5a0c3a0524c73d431894d2b2f156588a60
Ashwin Khode <ashwin.khode1@gmail.com>
2021-04-03 17:21:07 +0530

    add: cart, product, wishlist page added

---
+++ src/pages/products.js
@@ -0,0 +1,29 @@
+import {server} from '../../config'
+import ProductGrid from '../components/ProductGrid/ProductGrid'
+
+export async function getStaticProps(context) {
+
+    const res = await fetch(`${server}/api/products`)
+    const data = await res.json()
+
+    console.log(data)
+
+    return {
+        props: {
+            products: data,
+            // isLoading: !error && !data,
+            // isError: error
+        }
+    }
+}
+
+const Products = ({products, isLoading, isError}) => {
+
+    return (
+        <div>
+            <ProductGrid products={products} />
+        </div>
+    )
+}
+
+export default Products
