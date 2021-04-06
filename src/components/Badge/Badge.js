f0942be3f091a7f16011c23955e1b42e3a0e4cfa
Ashwin Khode <ashwin.khode1@gmail.com>
2021-04-07 01:52:21 +0530

    fix: Improved Badge

--- src/components/Badge/Badge.js
+++ src/components/Badge/Badge.js
@@ -1,6 +1,6 @@
 const Badge = ({query}) => {
     return (
-        <div className="absolute -top-2 right-0 sm:-top-1 sm:right-2 text-[0.5rem] text-white font-bold px-[4px] rounded-full bg-red-500 flex justify-center items-center">{query}</div>
+        <div className="absolute -top-0 right-0 sm:-top-0 sm:right-2 text-[0.5rem] text-white font-bold px-[4px] w-4 h-4 rounded-full bg-red-500 flex justify-center items-center">{query}</div>
     )
 }
 
