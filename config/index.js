2e586684f4cc5131bf99e6aeebaf30f8c8f42c72
Ashwin Khode <ashwin.khode1@gmail.com>
2021-04-09 03:03:01 +0530

    refactor: changed api url

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000/api/products' : 'https://fakestoreapi.com/products';
+export const server = dev ? 'http://localhost:3000/api/products' : 'https://dapr.vercel.app/api/products';
