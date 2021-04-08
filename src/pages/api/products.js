2bebd27266b487b36765c7c10339d47a9960ac23
Ashwin Khode <ashwin.khode1@gmail.com>
2021-04-09 03:04:56 +0530

    feat: added mrp in fetched products data

--- src/pages/api/products.js
+++ src/pages/api/products.js
@@ -1,9 +1,20 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+const dataModifer = async (array) => {
+
+  const pArray = array.map(async value => {
+    return {...value, mrp: Math.floor(await value.price + Math.random() * 100)}
+  })
+  const rArray = await Promise.all(pArray)
+
+  return rArray
+}
+
 export default async (req, res) => {
 
   const fetchData = await fetch('https://fakestoreapi.com/products')
   const response = await fetchData.json()
+  const result = await dataModifer(response)
 
-  res.status(200).json(response)
+  res.status(200).json(result)
 }
