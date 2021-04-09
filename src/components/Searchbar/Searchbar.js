c2aa23fee02ce05bfa995eb50b1a3dd94da2f3ea
Ashwin Khode <ashwin.khode1@gmail.com>
2021-04-09 06:09:23 +0530

    feat: Search functionality added

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,27 +1,48 @@
 import {useState} from 'react'
+import {useRouter} from 'next/router'
 import {AiOutlineSearch} from 'react-icons/ai'
 
 import Button from '../Button/Button'
 
 const Searchbar = () => {
     const [userInput, setUserInput] = useState('')
+    const router = useRouter()
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        router.push(`products?search=${userInput}`)
+    }
 
     return (
         <div
             className='flex flex-row justify-between items-center border-b-2 h-3/4 border-gray-400 rounded text-xs sm:text-base sm:max-w-full overflow-hidden '>
-            <input
-                className='h-full w-[90%] md:3/4 lg:w-5/6 outline-none p-4'
-                type='text'
-                placeholder='Search'
-                onChange={
-                    (e) => setUserInput(e.target.value)
-                }
-            />
-            <div className="h-full w-[15%] md:1/4 lg:w-2/12 rounded overflow-hidden">
-                <Button variant='primary' fullWidth className='w-full h-full' padding='p-0 px-1 py-0'>
-                    <AiOutlineSearch className='text-2xl' />
-                </Button>
-            </div>
+            <form type='submit' onSubmit={(e) => handleSubmit(e)} className='flex w-full h-full'>
+                <input
+                    className='h-full w-[90%] md:3/4 lg:w-5/6 outline-none p-4'
+                    type='text'
+                    placeholder='Search'
+                    onChange={
+                        (e) => setUserInput(e.target.value)
+                    }
+                // onSubmit={
+                //     (e) => handleSubmit(e)
+                // }
+
+                />
+                <div className="h-full w-[15%] md:1/4 lg:w-2/12 rounded overflow-hidden">
+                    <Button
+                        variant='primary'
+                        className='w-full h-full'
+                        padding='p-0 px-1 py-0'
+                        fullWidth
+                        onClick={
+                            (e) => handleSubmit(e)
+                        }
+                    >
+                        <AiOutlineSearch className='text-2xl' />
+                    </Button>
+                </div>
+            </form>
         </div>
     )
 }
---
+++ src/pages/products/index.js
@@ -0,0 +1,46 @@
+import {useRouter} from 'next/router'
+import Fuse from 'fuse.js'
+
+import {server} from '../../../config'
+
+import ProductGrid from '../../components/ProductGrid/ProductGrid'
+import SEO from '../../components/SEO/SEO'
+import SortFilter from '../../components/SortFilter/SortFilter'
+
+export async function getStaticProps(context) {
+
+    const res = await fetch(server)
+    const data = await res.json()
+
+    return {
+        props: {
+            products: data,
+            // isLoading: !error && !data,
+            // isError: error
+        }
+    }
+}
+
+const Products = ({products}) => {
+
+    const router = useRouter()
+    const searchQuery = decodeURI(router.asPath).split('=')[1]
+    const fuse = new Fuse(products, {keys: ['title']})
+    const searchedProducts = searchQuery
+        ? fuse.search(searchQuery).map(element => element.item)
+        : products
+    const totalProducts = searchedProducts.length
+
+    return (
+        <div className='relative pb-8 mt-2 md:mt-0'>
+            <SEO pageTitle='Products' />
+            <div className='flex justify-between items-center sm:-mx-4 z-20 fixed bottom-0 bg-white w-[100vw] sm:w-[98vw] lg:w-full lg:mx-0 lg:relative lg:mb-1 lg:shadow-none'>
+                <h1 className='font-bold text-2xl hidden lg:block'>Showing Products <small className='font-light text-sm ml-1'>{`(Showing 1 – ${totalProducts}  products of ${totalProducts} products)`}</small></h1>
+                <SortFilter />
+            </div>
+            <ProductGrid products={searchedProducts} />
+        </div>
+    )
+}
+
+export default Products
