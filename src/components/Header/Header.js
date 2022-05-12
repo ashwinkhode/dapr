b654a8d6bd56a2fa68f2f44fbc3268ee63ee381d
Ashwin Khode <ashwin.khode1@gmail.com>
2022-05-13 00:33:54 +0530

    upgrade: react, react-dom, next, taiwlind to latest

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,36 +1,37 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import clsx from 'clsx'
-import {useState} from 'react'
-import {AiOutlineShoppingCart, AiOutlineHeart, AiOutlineSearch} from 'react-icons/ai'
+import { useState } from 'react'
+import {
+    AiOutlineShoppingCart,
+    AiOutlineHeart,
+    AiOutlineSearch,
+} from 'react-icons/ai'
 
-import {useCart} from '../../context/cartContext'
+import { useCart } from '../../context/cartContext'
 
 import Badge from '../Badge/Badge'
 import Button from '../Button/Button'
 import Searchbar from '../Searchbar/Searchbar'
 
 const Header = () => {
-
-    const {cartState} = useCart()
+    const { cartState } = useCart()
 
     const [searchbarToggle, setSearchbarToggle] = useState(true)
 
     const visibility = searchbarToggle ? 'hidden' : ''
 
     return (
-        <div className='w-full mx-auto sticky inset-0 bg-white z-[100] shadow'>
-            <div className='w-[80%] mx-auto'>
-
+        <div className="w-full mx-auto sticky inset-0 bg-white z-[100] shadow">
+            <div className="w-[80%] mx-auto">
                 <div className="flex flex-row items-center justify-between w-full p-2 h-[10vh]">
-
-                    <Link href='/'>
-                        <a className='justify-center items-center flex'>
+                    <Link href="/">
+                        <a className="justify-center items-center flex">
                             <Image
-                                src='/logo.png'
-                                alt='Dapr'
-                                height='50'
-                                width='100'
+                                src="/logo.png"
+                                alt="Dapr"
+                                height="50"
+                                width="100"
                             />
                         </a>
                     </Link>
@@ -40,30 +41,45 @@ const Header = () => {
                     </div>
 
                     <div className="flex items-center">
-                        <Link href='/products'>
-                            <Button className='hidden sm:block'>
-                                SHOP
-                            </Button>
+                        <Link href="/products">
+                            <a>
+                                <Button className="hidden sm:block">
+                                    SHOP
+                                </Button>
+                            </a>
                         </Link>
-                        <Button className='sm:hidden' onClick={() => setSearchbarToggle(!searchbarToggle)}>
-                            <AiOutlineSearch className='text-lg sm:text-2xl ' />
+                        <Button
+                            className="sm:hidden"
+                            onClick={() => setSearchbarToggle(!searchbarToggle)}
+                        >
+                            <a>
+                                <AiOutlineSearch className="text-lg sm:text-2xl " />
+                            </a>
                         </Button>
-                        <Link href='/wishlist'>
-                            <Button>
-                                <Badge query={cartState.wishlist.length} />
-                                <AiOutlineHeart className='text-lg sm:text-2xl' />
-                            </Button>
+                        <Link href="/wishlist">
+                            <a>
+                                <Button>
+                                    <Badge query={cartState.wishlist.length} />
+                                    <AiOutlineHeart className="text-lg sm:text-2xl" />
+                                </Button>
+                            </a>
                         </Link>
-                        <Link href='/cart'>
-                            <Button>
-                                <Badge query={cartState.cart.length} />
-                                <AiOutlineShoppingCart className='text-lg sm:text-2xl' />
-                            </Button>
+                        <Link href="/cart">
+                            <a>
+                                <Button>
+                                    <Badge query={cartState.cart.length} />
+                                    <AiOutlineShoppingCart className="text-lg sm:text-2xl" />
+                                </Button>
+                            </a>
                         </Link>
                     </div>
-
                 </div>
-                <div className={clsx(visibility, 'w-full p-2 h-[10vh] sm:hidden')}>
+                <div
+                    className={clsx(
+                        visibility,
+                        'w-full p-2 h-[10vh] sm:hidden',
+                    )}
+                >
                     <Searchbar />
                 </div>
             </div>
--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -9,50 +9,62 @@ const VARIANTS = {
         div: 'bg-white',
         h1: 'text-gray-700',
         h2: 'text-gray-700',
-        p: 'text-gray-500'
+        p: 'text-gray-500',
     },
     gray: {
         div: 'bg-gray-700',
         h1: 'text-white',
         h2: 'text-white',
-        p: 'text-gray-300'
+        p: 'text-gray-300',
     },
 }
 
-const Jumbotron = ({title, subtitle, description, url, variant = 'white'}) => {
-
-    const {div, h1, h2, p} = VARIANTS[variant]
+const Jumbotron = ({
+    title,
+    subtitle,
+    description,
+    url,
+    variant = 'white',
+}) => {
+    const { div, h1, h2, p } = VARIANTS[variant]
 
     return (
-        <div className={
-            clsx(div, "h-[90vh] sm:h-[60vh] p-3 lg:p-4")
-        }>
+        <div className={clsx(div, 'h-[90vh] sm:h-[60vh] p-3 lg:p-4')}>
             <div className="border-2 border-white flex flex-col pt-6 gap-y-8 sm:flex-row justify-center items-center lg:justify-between h-full lg:px-16">
                 <div className="w-4/5 sm:w-1/2 ml-6 lg:ml-0 flex flex-col justify-center">
-                    <h1 className={clsx(h1, "font-light text-3xl lg:text-6xl")}>
+                    <h1 className={clsx(h1, 'font-light text-3xl lg:text-6xl')}>
                         {title}
                     </h1>
-                    <h2 className={clsx(h2, "font-bold text-2xl lg:text-4xl py-2 ")}>
+                    <h2
+                        className={clsx(
+                            h2,
+                            'font-bold text-2xl lg:text-4xl py-2 ',
+                        )}
+                    >
                         {subtitle}
                     </h2>
-                    <p className={clsx(p, "text-sm w-3/4 pt-2 pb-6 ")}>
+                    <p className={clsx(p, 'text-sm w-3/4 pt-2 pb-6 ')}>
                         {description}
                     </p>
-                    <Link href='/products'>
-                        <Button variant={variant === 'white' ? 'secondary' : 'invert'}
-                            className="border-2 w-max">
-                            <a>
+                    <Link href="/products">
+                        <a>
+                            <Button
+                                variant={
+                                    variant === 'white' ? 'secondary' : 'invert'
+                                }
+                                className="border-2 w-max"
+                            >
                                 Shop Now
-                            </a>
-                        </Button>
+                            </Button>
+                        </a>
                     </Link>
                 </div>
                 <div className="w-2/3 h-full lg:w-[40%]  md:w-2/5 relative overflow-hidden">
                     <Image
                         src={url}
-                        alt='clothing'
-                        layout='fill'
-                        objectFit='contain'
+                        alt="clothing"
+                        layout="fill"
+                        objectFit="contain"
                     />
                 </div>
             </div>
